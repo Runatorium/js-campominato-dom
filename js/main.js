cef63a985dbfc010ede112de530b577381a5057b
Runatorium <stefano.alesci1@gmail.com>
2023-03-06 12:24:55 +0100

    Saving files commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const gridDom = document.getElementById('grid');
+const btnDom = document.getElementById('action-button')
+
+btnDom.addEventListener('click', function(){
+
+    const difficoltà = document.getElementById('selezione-difficolta');
+    const dfc = difficoltà.value;
+    var nsquare;
+
+    switch(dfc){
+        case 'easy':
+        var nsquare = 100;
+        break;
+        
+        case 'hard':
+        var nsquare = 81;
+        break;
+
+        case 'insane':
+        var nsquare = 49;
+        break;
+
+    }
+         
+    gridDom.innerHTML = "";
+    creagriglia(nsquare);
+   
+});
+    
+
+    
+function creanuovoquadrato(){
+    const currentElement = document.createElement('div');
+    currentElement.classList.add('square');
+    return currentElement;
+}
+
+
+function creagriglia (nsquare){
+    for(i=0; i < nsquare; i++){
+        const quadratocorrente = creanuovoquadrato();
+        
+            quadratocorrente.addEventListener('click', function() {
+            this.classList.toggle('clicked');
+            console.log(quadratocorrente.innerHTML);
+           
+        });
+
+        gridDom.append(quadratocorrente);
+        quadratocorrente.append(i);
+            
+     }
+}; 
